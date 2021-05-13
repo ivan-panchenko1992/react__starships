04c91e3b19348cd67ebdcd183848723273ca932f
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-05-13 12:51:37 +0300

    fixedStyle

--- src/components/starShip/StarShip.tsx
+++ src/components/starShip/StarShip.tsx
@@ -13,12 +13,44 @@ interface Props {
 export const StarShip: React.FC<Props> = ({ starShip }) => (
   <Card className="star-ship__card card">
     <CardBody>
-      <CardTitle tag="h5">{starShip.name}</CardTitle>
-      <CardSubtitle tag="h6" className="mb-2 text-muted">{starShip.model}</CardSubtitle>
-      <CardText>{starShip.starshipClass}</CardText>
-      <CardText>{starShip.hyperdriveRating}</CardText>
-      <CardText>{starShip.passengers}</CardText>
-      <CardText>{starShip.manufacturer}</CardText>
+      <CardTitle
+        tag="h5"
+        className="card__text"
+        title={starShip.name}
+      >
+        {starShip.name}
+      </CardTitle>
+      <CardSubtitle
+        tag="h6"
+        className="mb-2 text-muted card__text"
+        title={starShip.model}
+      >
+        {starShip.model}
+      </CardSubtitle>
+      <CardText
+        className="card__text"
+        title={starShip.starshipClass}
+      >
+        {starShip.starshipClass}
+      </CardText>
+      <CardText
+        className="card__text"
+        title={starShip.hyperdriveRating}
+      >
+        {starShip.hyperdriveRating}
+      </CardText>
+      <CardText
+        title={starShip.passengers}
+        className="card__text"
+      >
+        {starShip.passengers}
+      </CardText>
+      <CardText
+        title={starShip.manufacturer}
+        className="card__text"
+      >
+        {starShip.manufacturer}
+      </CardText>
     </CardBody>
   </Card>
 );
